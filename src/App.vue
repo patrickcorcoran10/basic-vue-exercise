b276fbdcaa08e2f06c9a7d336e933aeda00a6e92
patrickcorcoran10 <patrickcorcoran10@gmail.com>
2021-04-05 14:44:22 -0500

    commit with finished code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PasswordChecker/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PasswordChecker from './components/PasswordChecker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PasswordChecker
   }
 }
 </script>
---
+++ src/components/PasswordChecker.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <label>Password Checker</label>
+    <br>
+    <input placeholder="Password" type="password" v-on:keyup.prevent="letterInput" v-model="password" v-bind:class="{badPassword : badPassword, goodPassword : goodPassword}">
+    <br>
+    <button @click="testSubmit">Test</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PasswordChecker',
+  data() {
+    return{
+      password: "",
+      goodPassword: false,
+      badPassword: true,
+      goodLength: false,
+      hasUpper: false,
+      hasLower: false,
+      hasSpecial: false
+    }
+  },
+  methods: {
+    letterInput() {
+      console.log("input", this.password)
+      if (this.password.length >= 5 && this.password.length <= 12) {
+          this.goodLength = true;
+      } else {
+        this.goodLength = false;
+      }
+      // Upper Case Conditional
+      if (/[A-Z]/.test(this.password)) {
+        this.hasUpper = true
+      } else {
+        this.hasUpper = false;
+      }
+      // Lower Case Conditional
+      if (/[a-z]/.test(this.password)) {
+        this.hasLower = true
+      } else {
+        this.hasLower = false;
+      }
+      // Special Character Conditional
+      if (/\W|_/.test(this.password)) {
+        this.hasSpecial = true
+      } else {
+        this.hasSpecial = false
+      }
+      // Conditional for the Rendering
+      if (this.goodLength && this.hasUpper && this.hasLower && this.hasSpecial) {
+        this.goodPassword = true;
+        this.badPassword = false;
+      } else {
+        this.goodPassword = false;
+        this.badPassword = true;
+      }
+    },
+    
+    testSubmit() {
+      if (this.goodPassword === true) {
+        alert("Success", this.password)
+        this.password = '';
+        this.goodPassword === false;
+        this.badPassword === true;
+      } else {
+        alert("Failure")
+        this.password = '';
+        this.goodPassword === false;
+        this.badPassword === true;
+      }
+    }
+  },
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input, .badPassword:focus {
+    outline: none !important;
+    border:1px solid red;
+    box-shadow: 0 0 10px #719ECE;
+}
+.goodPassword:focus {
+    outline: none !important;
+    border:1px solid green;
+    box-shadow: 0 0 10px #719ECE;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
